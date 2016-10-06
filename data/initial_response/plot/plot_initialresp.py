0522ef37fae7c7097f9e40a5cdcb528207e0f469
Marvin Boettcher <boettcher@evolbio.mpg.de>
2016-10-06 17:54:50 +0200

    Added plot script.

---
+++ data/initial_response/plot/plot_initialresp.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from matplotlib.ticker import MaxNLocator 
+
+import plothelpers
+import helperfunctions
+import sys
+
+plothelpers.latexify()
+fig,ax=plt.subplots(1,1)
+fig.subplots_adjust(left=0.19,bottom=0.20,right=0.96,top=0.96)
+ax.margins(0.05)
+
+filenamer=sys.argv[1]
+filenamen=sys.argv[2]
+relapse=np.loadtxt(filenamer)
+norelapse=np.loadtxt(filenamen)
+
+ax.hist(relapse[:,0],bins=50,normed=True,alpha=0.8,label="relapse")
+ax.hist(norelapse[:,0],bins=50,normed=True,alpha=0.8,label="no relapse")
+
+ax.xaxis.set_major_locator(MaxNLocator(6))
+ax.set_xlabel("initial response slope")
+ax.set_ylabel("probability density")
+
+ax.legend()
+plt.show()
