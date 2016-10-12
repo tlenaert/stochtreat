9e17bdbb7bba5421d5457f8b66eea6e9ec524b41
Marvin Boettcher <boettcher@evolbio.mpg.de>
2016-10-12 14:39:51 +0200

    Plot correlations with pandas.

---
+++ data/initial_response/plot/plot_correlations.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.stats 
+import pandas as pd
+from matplotlib.ticker import MaxNLocator 
+
+import plothelpers
+import helperfunctions
+import sys
+
+filename=sys.argv[1]
+data = pd.read_csv(filename, delim_whitespace=True, header = None,index_col=False)
+
+cols= ["$t_{nolsc}$", "$t_{diag}$","$t_{red}$","init resp","lsc at diag", "1y burden"]
+data.columns = cols
+
+cols[3], cols[0] = cols[0], cols[3]
+data=data.ix[:,cols]
+
+correl=data.corr().dropna(axis=(0,1),how='all') 
+print(correl)
+
+plothelpers.latexify()
+fig,ax=plt.subplots(1,1)
+fig.subplots_adjust(left=0.04,bottom=0.03,right=0.88,top=0.72)
+ax.margins(0.05)
+
+matplot=ax.matshow(correl,cmap="viridis")
+
+ax.set_yticklabels([""]+list(correl.columns))
+ax.set_xticklabels([""]+list(correl.columns), rotation=45)
+for tick in ax.xaxis.get_majorticklabels():
+    tick.set_horizontalalignment("left")
+
+cb=fig.colorbar(matplot)
+cb.set_label("Pearson corr. coeff.")
+
+
+plt.show()
