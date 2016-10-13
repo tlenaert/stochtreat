06a2a7231ed8f744146e029693715bd3eb720376
Marvin Boettcher <boettcher@evolbio.mpg.de>
2016-10-13 15:59:39 +0200

    Now fancy correlation plot, concatenating data beforehand on demand.

--- data/initial_response/plot/plot_correlations.py
+++ data/initial_response/plot/plot_correlations.py
@@ -22,15 +22,15 @@ data=data.ix[:,cols]
 cols=[col.replace("_","\_") for col in cols]
 data.columns=cols
 
-correl=data.corr().dropna(axis=(0,1),how='all') 
-print(correl.columns)
+correl=data.corr().dropna(axis=(0,1),how='all')# method="pearson"
+# print(correl.columns)
 
 plothelpers.latexify(columns=2,fig_height=4.5)
 fig,ax=plt.subplots(1,1)
 fig.subplots_adjust(left=0.20,bottom=0.03,right=0.88,top=0.72)
 ax.margins(0.05)
 
-matplot=ax.matshow(correl,cmap="viridis")
+matplot=ax.matshow(correl,cmap="inferno")
 
 ax.set_xticks(np.arange(len(correl.columns)))
 ax.set_yticks(np.arange(len(correl.columns)))
