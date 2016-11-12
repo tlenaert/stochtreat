737221c83e057656426b5544069882337238acf5
Marvin A. Böttcher <boettcher@evolbio.mpg.de>
2016-11-12 14:59:40 -0500

    Finalized data analysis script.

--- data/find_epsilon/analyze_find_epsilon.py
+++ data/find_epsilon/analyze_find_epsilon.py
@@ -4,35 +4,38 @@ import numpy as np
 import helperfunctions
 import sys
 
-array_to_find=np.array([0.00266636,0.00119456,0.000851222])
-delta=1e-4
+# array_to_find=np.array([0.00266636,0.00119456,0.000851222])
+array_to_find=np.array([0.00261841,0.00115792,0.000802662 ])
+delta=5e-6
 
+##get tested epsilon values
+epsilon_min=0.5
+epsilon_max=1.0
+epsilonvalues=20
 epsilon_i_range=np.linspace(epsilon_min,epsilon_max,epsilonvalues)
 epsilon_c_range=np.linspace(epsilon_min,epsilon_max,epsilonvalues)
+eps_list=[]
 for epsilon_c in epsilon_c_range:
     for epsilon_i in epsilon_i_range:
         if epsilon_i<epsilon_c:
             continue
+        eps_list.append([epsilon_c,epsilon_i])
+eps_list=np.array(eps_list)
 
+#process file data
 filenames = helperfunctions.natural_sort(sys.argv[1:])
-t_max=10.
-t_steps=100
-
-rawdata=[np.loadtxt(filename,comments="#")[:,0] for filename in filenames]
+rawdata=[[np.loadtxt(filename,comments="#")] for filename in filenames]
 data=np.concatenate(rawdata)
 
-# print(data)
-# exit(0)
-times=np.linspace(0.,t_max,t_steps)
+a=data <  array_to_find+delta
+b=data >  array_to_find-delta
+c=np.logical_and(a,b)
 
-lsc_frac=[]
-no_patients=np.nansum((data>=0.))
-
-for t in times:
-    no_lscs=np.nansum((data<t))
-    lsc_frac.append(1-no_lscs/float(no_patients))
-
-for t,l in zip(times,lsc_frac):
-    print(t,l)
+resultindeces=np.where(c.all(axis=1))
+result=eps_list[resultindeces]
+for y in result:
+    for x in y:
+        print(x,end=" ")
+    print()
 
 
