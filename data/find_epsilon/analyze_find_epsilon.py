dc2c984e5f2b8ed3f94ce130b891c9c11cf66a7b
Marvin Boettcher <boettcher@evolbio.mpg.de>
2016-11-14 18:45:57 +0100

    search for lowest deviation in search.

--- data/find_epsilon/analyze_find_epsilon.py
+++ data/find_epsilon/analyze_find_epsilon.py
@@ -12,14 +12,14 @@ to_find_data=[
 [0.119117897496,0.0311775891388,0.0391527108148], #euro LOW 0
 [0.0933576305015,0.0753268313524,0.0255725420811],#euro INT 0
 [0.502049958281,0.204397630799,0.108763321483]]  #euro HIGH 
-array_to_find=np.array(to_find_data[0])
+array_to_find=np.array(to_find_data[int(sys.argv[1])])
 
-delta=5e-6
+delta=5e-1
 
 ##get tested epsilon values
 epsilon_min=0.5
 epsilon_max=1.0
-epsilonvalues=30
+epsilonvalues=20
 epsilon_i_range=np.linspace(epsilon_min,epsilon_max,epsilonvalues)
 epsilon_c_range=np.linspace(epsilon_min,epsilon_max,epsilonvalues)
 eps_list=[]
@@ -31,24 +31,41 @@ for epsilon_c in epsilon_c_range:
 eps_list=np.array(eps_list)
 
 #process file data
-filenames = helperfunctions.natural_sort(sys.argv[1:])
+filenames = helperfunctions.natural_sort(sys.argv[2:])
 rawdata=[[np.loadtxt(filename,comments="#")] for filename in filenames]
 data=np.concatenate(rawdata)
 eps_list=eps_list[~np.isnan(data).any(axis=1)]
 data= data[~np.isnan(data).any(axis=1)]
 
-a=data <  array_to_find+delta
-b=data >  array_to_find-delta
-c=np.logical_and(a,b)
+def find_closest_values(data,array_to_find,delta):
+    a=data <  array_to_find+delta
+    b=data >  array_to_find-delta
+    c=np.logical_and(a,b)
+    
+    resultindices=np.where(c.all(axis=1))[0]
+    return(resultindices)
 
-resultindices=np.where(c.all(axis=1))
-print(resultindices)
-result=eps_list[resultindices]
+number=np.inf
+d=delta
+inx=[]
+while (number > 1):
+    oldinx=inx
+    inx=find_closest_values(data,array_to_find,d)
+    number=len(inx)
+    d=d-1e-5
+    if (number<1):
+        inx=oldinx
+resultindices=inx
+
+result_eps=eps_list[resultindices]
 resultdata=data[resultindices]
-print("#finding ",array_to_find)
-for y,yresult in zip(result,resultindices):
-    for x in y:
+
+print("#finding ",array_to_find,"precision="+str(d))
+for epslist,indice in zip(result_eps,resultindices):
+    for x in epslist:
         print(x,end=" ")
+    for y in data[indice]:
+        print(y,end=" ")
     print()
 
 
